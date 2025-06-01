af0d676fb5d25fedce255760e9da5e3f4063bac9
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-06-01 18:07:42 +0300

    add routes genartor

---
+++ lib/core/utils/config/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String forgotPassword = '/forgot-password';
+  static const String otp = '/otp';
+  static const String newPassword = '/new-password';
+}
---
+++ lib/core/utils/config/routes_genartor.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:gradproject/core/utils/config/routes.dart';
+import 'package:gradproject/features/auth/presentation/screens/login.dart';
+import 'package:gradproject/features/auth/presentation/screens/signup.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const Login());
+      case Routes.signup:
+        return MaterialPageRoute(builder: (_) => const Signup());
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No Route Found'),
+        ),
+        body: const Center(child: Text('No Route Found')),
+      ),
+    );
+  }
+}
