c07a7c4e8f92d9f47b7d8045b011f8a10960ae61
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-06-05 01:45:51 +0300

    edit in otp

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,4 +4,11 @@ class Endpoints {
   static const String confirmEmail = '/api/Account/ConfirmEmail';
   static const String forgotPassword =
       '/api/Account/ResendEmailConfirmationCode';
+  static const String requestResetPassword =
+      '/api/Account/RequestResetPassword';
+
+  static const String confirmResetPassword =
+      '/api/Account/ConfirmResetPasswordCode';
+
+  static const String resetPassword = 'api/Account/ResetPassword';
 }
