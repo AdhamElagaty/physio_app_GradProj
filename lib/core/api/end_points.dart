b0b6b8a1dda25bc9cce68d952b1976a3df679901
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-06-12 18:42:00 +0300

    update endpoints

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,14 +1,13 @@
 class Endpoints {
-  static const String signIn = '/api/Account/Login';
-  static const String signUp = '/api/Account/Register';
-  static const String confirmEmail = '/api/Account/ConfirmEmail';
+  static const String signIn = '/api/auth/login';
+  static const String signUp = '/api/auth/register';
+  static const String confirmEmail = '/api/auth/confirm-email';
   static const String forgotPassword =
-      '/api/Account/ResendEmailConfirmationCode';
-  static const String requestResetPassword =
-      '/api/Account/RequestResetPassword';
+      '/api/auth/resend-email-confirmation-code';
+  static const String requestResetPassword = '/api/user/password/request-reset';
 
-  static const String confirmResetPassword =
-      '/api/Account/ConfirmResetPasswordCode';
+  static const String confirmResetPassword = '/api/user/password/confirm-reset';
 
-  static const String resetPassword = '/api/Account/ResetPassword';
+  static const String resetPassword = 'api/user/password/reset';
+  static const String sendMessage = '/api/aibot/message';
 }
