845ffa517d336c5ed9c697adbac0716792987db7
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-06-13 17:50:37 +0300

    add ai message model

---
+++ lib/features/home/presentation/screens/chat_bot/data/models/ai_chat_message_model.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:gradproject/features/home/presentation/screens/chat_bot/data/models/ai_chat_model.dart';
+import 'package:intl/intl.dart' as intl;
+
+enum MessageRole { user, model }
+
+class AiChatMessage extends Equatable {
+  final String id;
+  final String content;
+  final MessageRole role;
+  final DateTime timestamp;
+  final bool isPending;
+  final bool isError;
+
+  const AiChatMessage({
+    required this.id,
+    required this.content,
+    required this.role,
+    required this.timestamp,
+    this.isPending = false,
+    this.isError = false,
+  });
+
+  factory AiChatMessage.fromJson(Map<String, dynamic> json, String id) {
+    return AiChatMessage(
+      id: id,
+      content: json['messageContent'],
+      role:
+          (json['roleType'] as int) == 1 ? MessageRole.user : MessageRole.model,
+      timestamp: AiChat.parseBackendDate(json['messageTime']),
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, content, role, timestamp, isPending, isError];
+}
