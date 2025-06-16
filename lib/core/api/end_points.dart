3f74896e164ed7d132b31c41eae20ef2160f281e
Adham Elagaty <adhamalagaty@gmail.com>
2025-06-16 16:00:43 +0300

    refactor Endpoints class to rename 'forgotPassword' to 'resendEmailConfirmation'

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -2,7 +2,7 @@ class Endpoints {
   static const String signIn = '/api/auth/login';
   static const String signUp = '/api/auth/register';
   static const String confirmEmail = '/api/auth/confirm-email';
-  static const String forgotPassword =
+  static const String resendEmailConfirmation =
       '/api/auth/resend-email-confirmation-code';
   static const String requestResetPassword = '/api/user/password/request-reset';
 
