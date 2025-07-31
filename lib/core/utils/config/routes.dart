f5e56843d42dd70eacca3ad521eb003aaca3a6f5
Adham Elagaty <adhamalagaty@gmail.com>
2025-07-31 18:38:56 +0300

    restructure and expand app routes with grouped home paths, new screens, and helper for home route identification

--- lib/core/utils/config/routes.dart
+++ lib/core/utils/config/routes.dart
@@ -1,8 +1,21 @@
 class Routes {
   static const String login = '/login';
+  static const String mainScreen = '/main-screen';
+  static const String guestHome = '/guest-home';
   static const String signup = '/signup';
   static const String forgotPassword = '/forgot-password';
   static const String otp = '/otp';
   static const String newPassword = '/new-password';
-  static const String home = '/home';
+
+  // home
+  static const String _baseHome = '/home';
+  static const String exerciseFilter = '$_baseHome/exercise-filter';
+  static const String exerciseDescription = '$_baseHome/exercise-description';
+  bool isRouteHomeParent(String? route){
+    return route != null && route.startsWith(_baseHome);
+  }
+
+  // chat bot
+  static const String _baseChatBot = '/chat-bot-history';
+  static const String chatBotChatScreen = '$_baseChatBot/chat-screen';
 }
